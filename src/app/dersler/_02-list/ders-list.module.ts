a8e5520decee061460479e83cce267caaa3c085a
Savas Karaduman <savas.karaduman@keytours.com>
2022-11-16 23:39:14 +0300

    unselect item ve getData from datasource finished

--- src/app/dersler/_02-list/ders-list.module.ts
+++ src/app/dersler/_02-list/ders-list.module.ts
@@ -4,11 +4,13 @@ import { DersListRoutingModule } from './ders-list-routing.module';
 import { DxListModule } from 'devextreme-angular';
 import { ListgenelComponent } from './_01-genel/listgenel.component';
 import { ListgetsetComponent } from './_02-get-set/listgetset.component';
+import { ListunselectitemComponent } from './_03-unselect-item/listunselectitem.component';
 
 @NgModule({
   declarations: [
     ListgenelComponent,
-    ListgetsetComponent
+    ListgetsetComponent,
+    ListunselectitemComponent
   ],
   imports: [
     CommonModule,
